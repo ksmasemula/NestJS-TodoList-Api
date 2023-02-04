1b69ab0fd34ed05146dcacef2b3c517fc4500497
Kabelo Masemula <ksmasemula@gmail.com>
2023-02-05 01:30:05 +0200

    using dependency injection for services and repositories

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,12 +4,7 @@ import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    tasksService: TasksService;
-
-    constructor(){
-
-        this.tasksService = new TasksService();
-    }
+    constructor(public tasksService: TasksService){}
 
     @Get()
     listTasks() { 
